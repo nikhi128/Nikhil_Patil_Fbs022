98b492a5b892507b9f4d3daa7153cb4dafa123bf
nikhi128 <147272759+nikhi128@users.noreply.github.com>
2025-05-31 13:08:50 +0530

    Add files via upload

---
+++ rectangle.area.c
@@ -0,0 +1,15 @@
+//area of rectange = h * width 
+
+void main(){
+
+	int height,width,area;
+	
+	height = 23;
+	width = 45;
+	area = height * width;
+
+	printf("%d","the area of rectangle is ",area);
+	
+	
+	
+}
---
+++ square-cube.c
@@ -0,0 +1,11 @@
+void main()
+{
+	int a=10;
+	
+	int square=a*a;
+	int cube=a*a*a;
+	
+	printf(" The square of  a is %d \n",square);
+		printf(" The cube of a is  %d ",cube);
+
+}
