8bc3cace7bf31c792a5bdb1900a1ed7afc79b3d7
nikhi128 <147272759+nikhi128@users.noreply.github.com>
2025-06-13 14:01:24 +0530

    Add files via upload

---
+++ Cprograming/Test/Test-2/positive-or-negative.c
@@ -0,0 +1,19 @@
+void main()
+{
+	int a;
+	printf(" Enter number :-");
+	scanf("%d",&a);
+	
+	if(a>0)
+	{
+	printf(" The number is positive");	
+	}
+   	else if(a<-0)
+	{
+		printf("The number is negative");
+	}
+	else
+	{
+		printf(" Number is neutral");
+	}
+}
